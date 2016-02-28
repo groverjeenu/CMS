d4480df01f758de849425f2508a345374300f558
groverjeenu <groverjeenu@gmail.com>
2016-02-28 11:03:24 +0530

    vfg

--- application/migrations/20160225195542_add_image_user.php
+++ application/migrations/20160225195542_add_image_user.php
@@ -12,7 +12,7 @@ class Migration_Add_image_user extends CI_Migration
 	
 	public function up()
 	{
-	    $query = "ALTER TABLE `CMS`.`users` ADD column `image` VARCHAR(100) NOT NULL default 'user.png';";
+	    $query = "ALTER TABLE `users` ADD column `image` VARCHAR(100) NOT NULL default 'user.png';";
 		$this->db->query($query);
     }
     
