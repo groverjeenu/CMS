fc3cf0af2a1a66fe5a53923b39899430beda017e
aishikchakraborty <aishik@Aishiks-MacBook-Air.local>
2016-03-02 21:57:06 +0530

    ahbsajja

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
+		error_reporting(E_ALL ^ E_WARNING);
 		ini_set('display_errors', 1);
 	break;
 
@@ -75,7 +75,7 @@ switch (ENVIRONMENT)
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED );
 		}
 		else
 		{
