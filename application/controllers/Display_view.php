26648834d11931dc92f5e8a2fa9b42b2b9b403c2
aishikchakraborty <aishik@Aishiks-MacBook-Air.local>
2016-03-06 15:26:46 +0530

    dashboard

--- application/controllers/Display_view.php
+++ application/controllers/Display_view.php
@@ -181,30 +181,61 @@ class Display_view extends CI_Controller {
 
 	public function course($cid)
 	{
-		if(!$this->ion_auth->logged_in())
+		if($this->ion_auth->in_group('faculty'))
 		{
-			redirect("login","refresh");
+			if(!$this->ion_auth->logged_in())
+			{
+				redirect("login","refresh");
+			}
+
+			$query = $this->courses->get_course($cid);
+			$data['query'] = $query;
+				
+
+			$usr= $this->ion_auth->user()->row();
+			$data['user']= (array)$usr;
+
+			$val = $this->courses->check_if_enrolled($cid);
+			$val_ca = $this->courses->is_ca($cid);
+			
+			$lectures = $this->courses->get_course_lectures($cid);
+			$data['val'] = $val;
+			$data['val_ca'] = $val_ca;
+			$data['lectures'] = $lectures;
+			$data['assignments'] = $this->courses->get_course_assignments($cid);
+			$data['grades_course']=$this->courses->get_course_grades($cid);
+			$data['wt'] = $this->courses->get_total_weight($cid);
+
+			$this->load->view('faculty_course',$data);	
 		}
 
-		$query = $this->courses->get_course($cid);
-		$data['query'] = $query;
+		else
+		{
+			if(!$this->ion_auth->logged_in())
+			{
+				redirect("login","refresh");
+			}
+
+			$query = $this->courses->get_course($cid);
+			$data['query'] = $query;
+				
+
+			$usr= $this->ion_auth->user()->row();
+			$data['user']= (array)$usr;
+
+			$val = $this->courses->check_if_enrolled($cid);
+			$val_ca = $this->courses->is_ca($cid);
 			
+			$lectures = $this->courses->get_course_lectures($cid);
+			$data['val'] = $val;
+			$data['val_ca'] = $val_ca;
+			$data['lectures'] = $lectures;
+			$data['assignments'] = $this->courses->get_course_assignments($cid);
+			$data['grades_course']=$this->courses->get_course_grades($cid);
+			$data['wt'] = $this->courses->get_total_weight($cid);
 
-		$usr= $this->ion_auth->user()->row();
-		$data['user']= (array)$usr;
-
-		$val = $this->courses->check_if_enrolled($cid);
-		$val_ca = $this->courses->is_ca($cid);
-		
-		$lectures = $this->courses->get_course_lectures($cid);
-		$data['val'] = $val;
-		$data['val_ca'] = $val_ca;
-		$data['lectures'] = $lectures;
-		$data['assignments'] = $this->courses->get_course_assignments($cid);
-		$data['grades_course']=$this->courses->get_course_grades($cid);
-		$data['wt'] = $this->courses->get_total_weight($cid);
-
-		$this->load->view('coursepage',$data);
+			$this->load->view('coursepage',$data);
+		}
 
 	}
 
